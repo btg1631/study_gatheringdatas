6792698ab08d20f0a52f2b7c39d11f6291980273
youngji Jang <btg1631@naver.com>
2023-12-22 17:41:13 +0900

    push

--- docs/quests/github_login_events_selector copy.py
+++ docs/quests/github_login_events_selector copy.py
@@ -12,20 +12,27 @@ browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
 capabilities = browser.capabilities
 
 # - 주소 입력
-browser.get("https://www.w3schools.com/")
+browser.get("https://www.jobplanet.co.kr/users/sign_in?_nav=gb")
 
 # - 가능 여부에 대한 OK 받음
 pass
 
 # - html 파일 받음(and 확인)
 html = browser.page_source
-# print(html)
 
 # - 정보 획득
 from selenium.webdriver.common.by import By
 
+element_login_field = browser.find_element(by=By.CSS_SELECTOR, value="#user_email")
+element_login_field.send_keys("btg1631@naver.com")
+
+element_password_field = browser.find_element(by=By.CSS_SELECTOR, value="#user_password")
+element_password_field.send_keys("******")
+
+element_login_button = browser.find_element(by=By.CSS_SELECTOR, value="div > section.section_email.er_r > fieldset > button")
+element_login_button.click()
+
 pass
-# browser.save_screenshot('./formats.png')
 
 # 브라우저 종료
 browser.quit()
