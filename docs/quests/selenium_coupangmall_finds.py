2f33059939bd0f00998ef58e7814535d86e116cc
youngji Jang <btg1631@naver.com>
2023-12-22 12:22:13 +0900

    push

--- docs/quests/selenium_coupangmall_finds.py
+++ docs/quests/selenium_coupangmall_finds.py
@@ -1,25 +1,15 @@
-# * 웹 크롤링 동작
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-
 webdriver_manager_directory = ChromeDriverManager().install()
 browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
-
-# ChromeDriver 실행
-
-# Chrome WebDriver의 capabilities 속성 사용
 capabilities = browser.capabilities
 
 # - 주소 입력
 browser.get("https://www.coupang.com/np/categories/194282")
 
-# - 가능 여부에 대한 OK 받음
-pass
-
-# - html 파일 받음(and 확인)
+# - html 파일 받음
 html = browser.page_source
-print(html)
 
 # - 정보 획득
 from selenium.webdriver.common.by import By
