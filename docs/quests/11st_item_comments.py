49a59ebf09fb74c49fe44f29b21e586682bad278
youngji Jang <btg1631@naver.com>
2023-12-27 17:54:54 +0900

    11st

--- docs/quests/11st_item_comments.py
+++ docs/quests/11st_item_comments.py
@@ -20,121 +20,129 @@ def Connectdb(collection_name):
     return collection
 
 # - 주소 입력
-browser.get("https://deal.11st.co.kr/browsing/DealAction.tmall?method=getShockingDealMain")
+url = 'https://www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain&xfrom=main^gnb'
+browser.get(url)
 
 # - 정보 획득
 from selenium.webdriver.common.by import By
 
-selector_value = "div.c-card-item.c-card-item--collection.c-card-item--box > a"
-element_bundle = browser.find_elements(by=By.CSS_SELECTOR, value=selector_value)
-for element_item in element_bundle[0:4]:
-    element_item.click()    # 상품 클릭
-    time.sleep(1)
+def Element(collection1, collection2):
+    element_companies = browser.find_elements(by=By.CSS_SELECTOR, value="div > a > span.best")
+    for index in range(4) :
+        element_companies = browser.find_elements(by=By.CSS_SELECTOR, value="div > a > span.best")
+        element_companies[index].click()    # 상품 클릭
+        time.sleep(1)
 
-    # 상품 이름
-    try:
-        element_title = browser.find_element(by=By.CSS_SELECTOR, value="h1.title")
-        title = element_title.text
-    except:
-        title = "None"
-    # 상품 이미지
-    try:
-        element_image = browser.find_element(by=By.CSS_SELECTOR, value="div.img_full.img_full_height > img")
-        image = element_image.get_attribute('src')
-    except:
-        element_image = browser.find_element(by=By.CSS_SELECTOR, value="div.img_full.img_full_width > img")
-        image = element_image.get_attribute('src')
-    # 상품 원가
-    try:
-        element_oldprice = browser.find_element(by=By.CSS_SELECTOR, value="dd.price_regular")
-        oldprice = element_oldprice.text
-    except:
-        oldprice = "None"    
-    # 상품 판매가
-    try:
-        element_price = browser.find_element(by=By.CSS_SELECTOR, value="dd.price > strong > span.value")
-        price = element_price.text
-    except:
-        price = "None"
-
-    rows = browser.find_elements(by=By.CSS_SELECTOR, value="#tabpanelDetail1 > table > tbody > tr")
-    # 상품 정보
-    data = []
-    for row in rows:
-        # 각 행의 모든 열 가져오기
-        cols = row.find_elements(By.TAG_NAME, 'td')
-        # 각 열의 텍스트 정보 가져오기
-        cols_text = [col.text for col in cols]
-        data.append(cols_text)
-
-    # db에 저장
-    collection = Connectdb("11st_item")
-    collection.insert_one({"element_number": data[0][1], "title" : title, "image" : image, "oldprice" : oldprice, "price" : price})
-    # "state" : data[0][0], "delivery" : data[1][1], "origin" : data[2][1]
-
-
-    # 리뷰 더보기 클릭
-    # while True:
-    #     try:
-    #         browser.find_element(by=By.CSS_SELECTOR, value="#review-list-page-area > div > button").click()
-    #     except:
-    #         break
-    #     time.sleep(1)
-
-    # iframe 으로 전환
-    browser.switch_to.frame("ifrmReview")
-
-    # 댓글
-    collection = Connectdb("11st_item_comments")
-    selector_value = "li.review_list_element"
-    element_bundle = browser.find_elements(by=By.CSS_SELECTOR, value=selector_value)
-    for element_item in element_bundle[0:3]:
-        # 작성자
+        # 상품 이름
         try:
-            selector_value_username = "dt.name"
-            element_username = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_username)
-            username = element_username.text
+            element_title = browser.find_element(by=By.CSS_SELECTOR, value="h1.title")
+            title = element_title.text
         except:
-            username = "None"
-        # 선택 옵션
+            title = "None"
+        # 상품 이미지
         try:
-            selector_value_option = "div.option > dd"
-            element_option = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_option)
-            option = element_option.text
+            element_image = browser.find_element(by=By.CSS_SELECTOR, value="#productImg > div > img")
+            image = element_image.get_attribute('src')
         except:
-            selector_value_option = "p.option"
-            element_option = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_option)
-            option = element_option.text
-        # 별점
+            element_image = browser.find_element(by=By.CSS_SELECTOR, value="div.img_full.img_full_height > img")
+            image = element_image.get_attribute('src')
+        # 상품 원가
         try:
-            selector_value_score = "div > p.grade > span > em"
-            element_score = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_score)
-            score = element_score.text
+            element_oldprice = browser.find_element(by=By.CSS_SELECTOR, value="dd.price_regular")
+            oldprice = element_oldprice.text
         except:
-            score = "None"
-        # 내용
+            oldprice = "None"    
+        # 상품 판매가
         try:
-            button = element_item.find_element(by=By.CSS_SELECTOR, value="button.c_product_btn.c_product_btn_more6.review-expand-open-text")
-            button.click()  # 더보기 버튼 클릭
-            time.sleep(1)
+            element_price = browser.find_element(by=By.CSS_SELECTOR, value="dd.price > strong > span.value")
+            price = element_price.text
+        except:
+            price = "None"
 
-            selector_value_review = "div.cont_text_wrap.active > p"
-            element_review = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_review)
-            review = element_review.text
-        except:
-            # 버튼이 없는 경우
-            selector_value_review = "div.cont_text_wrap > p"
-            element_review = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_review)
-            review = element_review.text
+        rows = browser.find_elements(by=By.CSS_SELECTOR, value="#tabpanelDetail1 > table > tbody > tr")
+        # 상품 정보
+        data = []
+        for row in rows:
+            # 각 행의 모든 열 가져오기
+            cols = row.find_elements(By.TAG_NAME, 'td')
+            # 각 열의 텍스트 정보 가져오기
+            cols_text = [col.text for col in cols]
+            data.append(cols_text)
 
         # db에 저장
-        collection.insert_one({"element_number" : data[0][1], "user_name" : username, "option" : option, "score" : score, "review" : review})
+        
+        try:
+            element_result = collection1.insert_one({"element_number": data[0][1], "title" : title, "image" : image, "oldprice" : oldprice, "price" : price})
+            element_id = element_result.inserted_id
+            # "state" : data[0][0], "delivery" : data[1][1], "origin" : data[2][1]
+        except:
+            element_result = collection1.insert_one({"title" : title, "image" : image, "oldprice" : oldprice, "price" : price, "Expiration_date" : data[0][0]})
+            element_id = element_result.inserted_id
 
-    browser.back()    # 뒤로가기
-    time.sleep(2)
-    pass
-pass
+        # 리뷰 더보기 클릭
+        # while True:
+        #     try:
+        #         browser.find_element(by=By.CSS_SELECTOR, value="#review-list-page-area > div > button").click()
+        #     except:
+        #         break
+        #     time.sleep(1)
 
+        # iframe 으로 전환
+        browser.switch_to.frame("ifrmReview")
+
+        # 댓글
+
+        selector_value = "li.review_list_element"
+        element_bundle = browser.find_elements(by=By.CSS_SELECTOR, value=selector_value)
+        for element_item in element_bundle[0:3]:
+            # 작성자
+            try:
+                selector_value_username = "dt.name"
+                element_username = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_username)
+                username = element_username.text
+            except:
+                username = "None"
+            # 선택 옵션
+            try:
+                selector_value_option = "div.option > dd"
+                element_option = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_option)
+                option = element_option.text
+            except:
+                selector_value_option = "p.option"
+                element_option = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_option)
+                option = element_option.text
+            # 별점
+            try:
+                selector_value_score = "div > p.grade > span > em"
+                element_score = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_score)
+                score = element_score.text
+            except:
+                score = "None"
+            # 내용
+            try:
+                button = element_item.find_element(by=By.CSS_SELECTOR, value="button.c_product_btn.c_product_btn_more6.review-expand-open-text")
+                button.click()  # 더보기 버튼 클릭
+                time.sleep(1)
+
+                selector_value_review = "div.cont_text_wrap.active > p"
+                element_review = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_review)
+                review = element_review.text
+            except:
+                # 버튼이 없는 경우
+                selector_value_review = "div.cont_text_wrap > p"
+                element_review = element_item.find_element(by=By.CSS_SELECTOR, value=selector_value_review)
+                review = element_review.text
+
+            # db에 저장
+            collection2.insert_one({"element_id" : element_id, "user_name" : username, "option" : option, "score" : score, "review" : review})
+
+        browser.back()    # 뒤로가기
+        time.sleep(2)
+        pass
+
+collection1 = Connectdb("11st_item")
+collection2 = Connectdb("11st_item_comments")
+Element(collection1, collection2)
 
 # 브라우저 종료
 browser.quit()
