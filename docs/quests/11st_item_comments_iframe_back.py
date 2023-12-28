550dce9c296b9e5e90a57e8b9f5371157cb495a3
youngji Jang <btg1631@naver.com>
2023-12-28 10:34:41 +0900

    11st_item

--- docs/quests/11st_item_comments_iframe_back.py
+++ docs/quests/11st_item_comments_iframe_back.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 import time
 webdriver_manager_directory = ChromeDriverManager().install()
 
@@ -19,35 +20,29 @@ def Connectdb(collection_name):
     collection = database[collection_name]
     return collection
 
-# - 주소 입력
-url = 'https://www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain&xfrom=main^gnb'
-browser.get(url)
-
-# - 정보 획득
-from selenium.webdriver.common.by import By
-
+# 상품 정보
 def Element(index, collection1):
     element_companies = browser.find_elements(by=By.CSS_SELECTOR, value="div > a > span.best")
     element_companies[index].click()    # 상품 클릭
     time.sleep(1)
-
-    try:    # 상품 이름
+    
+    try:        # 상품 이름
         element_title = browser.find_element(by=By.CSS_SELECTOR, value="h1.title")
         title = element_title.text
     except:
         title = "None"
-    try:    # 상품 이미지
+    try:        # 상품 이미지
         element_image = browser.find_element(by=By.CSS_SELECTOR, value="#productImg > div > img")
         image = element_image.get_attribute('src')
     except:
         element_image = browser.find_element(by=By.CSS_SELECTOR, value="div.img_full.img_full_height > img")
         image = element_image.get_attribute('src')
-    try:    # 상품 원가
+    try:        # 상품 원가
         element_oldprice = browser.find_element(by=By.CSS_SELECTOR, value="dd.price_regular")
         oldprice = element_oldprice.text
     except:
         oldprice = "None"    
-    try:    # 상품 판매가
+    try:        # 상품 판매가
         element_price = browser.find_element(by=By.CSS_SELECTOR, value="dd.price > strong > span.value")
         price = element_price.text
     except:
@@ -55,12 +50,12 @@ def Element(index, collection1):
 
     rows = browser.find_elements(by=By.CSS_SELECTOR, value="#tabpanelDetail1 > table > tbody > tr")
     data = []
-    for row in rows:    # 상품 정보
+    for row in rows:        # 상품 정보
         cols = row.find_elements(By.TAG_NAME, 'td')        # 각 행의 모든 열 가져오기
         cols_text = [col.text for col in cols]        # 각 열의 텍스트 정보 가져오기
         data.append(cols_text)
 
-    try:    # db에 저장
+    try:        # db에 저장
         element_result = collection1.insert_one({"element_number": data[0][1], "title" : title, "image" : image, "oldprice" : oldprice, "price" : price})
         element_id = element_result.inserted_id
         # "state" : data[0][0], "delivery" : data[1][1], "origin" : data[2][1]
@@ -69,6 +64,7 @@ def Element(index, collection1):
         element_id = element_result.inserted_id
     return element_id
 
+# 리뷰
 def Review(element_id, collection2):
         # while True:        # 리뷰 더보기 클릭
         #     try:
@@ -80,7 +76,6 @@ def Review(element_id, collection2):
         browser.switch_to.frame("ifrmReview")        # iframe 으로 전환
         time.sleep(2)
 
-        # 댓글
         selector_value = "li.review_list_element"
         element_bundle = browser.find_elements(by=By.CSS_SELECTOR, value=selector_value)
         time.sleep(1)
@@ -125,8 +120,12 @@ def Review(element_id, collection2):
             collection2.insert_one({"element_id" : element_id, "user_name" : username, "option" : option, "score" : score, "review" : review})
         pass
 
-collection1 = Connectdb("11st_item11")
-collection2 = Connectdb("11st_item_comments11")
+# 주소 입력
+url = 'https://www.11st.co.kr/browsing/BestSeller.tmall?method=getBestSellerMain&xfrom=main^gnb'
+browser.get(url)
+
+collection1 = Connectdb("11st_item")
+collection2 = Connectdb("11st_item_comments")
 
 for index in range(4) :
     element_id = Element(index, collection1)
